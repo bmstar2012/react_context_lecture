8bdd9a1f76a9d325f9d1757a68c71d2f19172f1e
bmstar <bmstar2012@gmail.com>
2021-11-19 00:13:19 +0800

    Added reducer

--- src/components/PlusButton/index.tsx
+++ src/components/PlusButton/index.tsx
@@ -1,17 +1,25 @@
 import {useContext, useEffect, useState} from 'react';
-import { CountContext } from '../../context/count';
+import {CountContext, useConfigDispatch, ConfigStatus as CountConfigStatus} from '../../context/count';
 
 export const PlusButton = () => {
-    console.log("useEffect");
-  const { plusCount } = useContext(CountContext);
-  const [currency, setCurrency] = useState(0);
-  useEffect(() => {
-      console.log("useEffect", currency);
-  })
+    console.log("button - useEffect");
+    const {plusCount} = useContext(CountContext);
+    const [currency, setCurrency] = useState(0);
+    useEffect(() => {
+        console.log("button - useEffect", currency);
+    })
+    const dispatch = useConfigDispatch();
 
-  return (
-      <>
-      <button onClick={plusCount}>+ 1</button>
-      <button onClick={() => setCurrency(currency + 1)}>+ 1</button>
-      </>);
+    return (
+        <>
+            <div>
+                <button onClick={plusCount}>+ 1</button>
+            </div>
+            <div>
+                <button onClick={() => setCurrency(currency + 1)}>+ 1</button>
+            </div>
+            <div>
+                <button onClick={() => {dispatch({status: CountConfigStatus.Initialized})}}>Update status</button>
+            </div>
+        </>);
 };
--- src/context/count/index.tsx
+++ src/context/count/index.tsx
@@ -1,30 +1,120 @@
-import { createContext, useState } from 'react';
+import {createContext, useState, useReducer, useContext} from 'react';
 
 const CountContext = createContext({
     count: 0,
-    plusCount: () => {},
+    plusCount: () => {
+    },
 });
 
 interface Props {
     children: JSX.Element | JSX.Element[];
 }
 
-const CountProvider = ({ children }: Props): JSX.Element => {
+export enum ConfigStatus {
+    Initialized,
+    Fetching,
+    Ready,
+    Failure,
+}
+
+interface Config {
+    number: number;
+    init: boolean;
+}
+
+interface State {
+    status: ConfigStatus;
+    config?: Config;
+}
+
+interface Initialized {
+    status: ConfigStatus.Initialized;
+    config?: Config;
+}
+
+interface Fetching {
+    status: ConfigStatus.Fetching;
+}
+
+interface Ready {
+    status: ConfigStatus.Ready;
+}
+
+interface Failure {
+    status: ConfigStatus.Failure;
+    config?: undefined;
+}
+
+export type Action = Initialized | Fetching | Ready | Failure;
+
+function configReducer(state: State, action: Action): State {
+    console.log("configReducer", state, action);
+    switch (action.status) {
+        case ConfigStatus.Ready:
+        case ConfigStatus.Initialized: {
+            return {...state, ...action};
+        }
+        case ConfigStatus.Failure: {
+            if (state.status === ConfigStatus.Fetching) {
+                return {...state, ...action};
+            } else {
+                return state;
+            }
+        }
+        case ConfigStatus.Fetching: {
+            return {
+                ...state,
+                ...action,
+            };
+        }
+    }
+}
+
+type Dispatch = (action: Action) => void;
+interface StateContextType {
+    state: State;
+    dispatch: Dispatch;
+}
+// const StateContext = createContext<State | undefined>(undefined);
+// const StateContext = createContext<StateContextType>({state: {status: ConfigStatus.Fetching}, dispatch: (action: Action) => {}});
+const StateContext = createContext<StateContextType|undefined>(undefined);
+
+// const DispatchContext = createContext<Dispatch | undefined>(undefined);
+const CountProvider = ({children}: Props): JSX.Element => {
     const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(configReducer, {
+        status: ConfigStatus.Fetching,
+    });
+    console.log('CountProvider', state);
 
     const plusCount = (): void => {
         setCount(count + 1);
     };
 
     return (
-        <CountContext.Provider
-            value={{
-                count,
-                plusCount,
-            }}>
-            {children}
-        </CountContext.Provider>
+        <StateContext.Provider value={{state, dispatch}}>
+            <CountContext.Provider
+                value={{
+                    count,
+                    plusCount,
+                }}>
+                {children}
+            </CountContext.Provider>
+        </StateContext.Provider>
     );
 };
 
-export { CountContext, CountProvider };
+function useConfigDispatch() {
+    const stateContext = useContext(StateContext);
+    const dispatch = stateContext?.dispatch;
+    if (!dispatch) {
+        throw new Error(`invalid dispatch`);
+    }
+
+    // return () => {dispatch({
+    //     status: ConfigStatus.Ready
+    // })};
+    return dispatch;
+}
+
+export {CountContext, CountProvider, useConfigDispatch};
