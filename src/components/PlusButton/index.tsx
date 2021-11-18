847ab69a53503f8770389c27b6dfcd6800c8f564
bmstar <bmstar2012@gmail.com>
2021-11-18 08:48:47 +0800

    example1

--- src/components/PlusButton/index.tsx
+++ src/components/PlusButton/index.tsx
@@ -1,8 +1,16 @@
-import { useContext } from 'react';
+import {useContext, useEffect, useState} from 'react';
 import { CountContext } from '../../context/count';
 
 export const PlusButton = () => {
   const { plusCount } = useContext(CountContext);
+  const [currency, setCurrency] = useState(0);
+  useEffect(() => {
+      console.log("useEffect", currency);
+  })
 
-  return <button onClick={plusCount}>+ 1</button>;
+  return (
+      <>
+      <button onClick={plusCount}>+ 1</button>
+      <button onClick={() => setCurrency(currency + 1)}>+ 1</button>
+      </>);
 };
