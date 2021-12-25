ff51e80cd55a75a066704fbbe0f1af1684926903
bmstar <bmstar2012@gmail.com>
2021-12-25 12:51:39 +0800

    added eslint

--- src/components/PlusButton/index.tsx
+++ src/components/PlusButton/index.tsx
@@ -1,25 +1,27 @@
-import {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {CountContext, useConfigDispatch, ConfigStatus as CountConfigStatus} from '../../context/count';
 
 export const PlusButton = () => {
-    console.log("button - useEffect");
-    const {plusCount} = useContext(CountContext);
-    const [currency, setCurrency] = useState(0);
-    useEffect(() => {
-        console.log("button - useEffect", currency);
-    })
-    const dispatch = useConfigDispatch();
+  console.log("button - useEffect");
+  const {plusCount} = useContext(CountContext);
+  const [currency, setCurrency] = useState(0);
+  useEffect(() => {
+    console.log("button - useEffect", currency);
+  })
+  const dispatch = useConfigDispatch();
 
-    return (
-        <>
-            <div>
-                <button onClick={plusCount}>+ 1</button>
-            </div>
-            <div>
-                <button onClick={() => setCurrency(currency + 1)}>+ 1</button>
-            </div>
-            <div>
-                <button onClick={() => {dispatch({status: CountConfigStatus.Initialized})}}>Update status</button>
-            </div>
-        </>);
+  return (<>
+    <div>
+      <button type='button' onClick={plusCount}>+ 1</button>
+    </div>
+    <div>
+      <button type='button' onClick={() => setCurrency(currency + 1)}>+ 1</button>
+    </div>
+    <div>
+      <button type='button' onClick={() => {
+        dispatch({status: CountConfigStatus.Initialized})
+      }}>Update status
+      </button>
+    </div>
+  </>);
 };
--- src/context/count/index.tsx
+++ src/context/count/index.tsx
@@ -1,120 +1,125 @@
-import {createContext, useState, useReducer, useContext} from 'react';
+import React, {createContext, useState, useReducer, useContext} from 'react';
+
 
 const CountContext = createContext({
-    count: 0,
-    plusCount: () => {
-    },
+  count: 0,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  plusCount: () => {
+  },
 });
 
 interface Props {
-    children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[];
 }
 
 export enum ConfigStatus {
-    Initialized,
-    Fetching,
-    Ready,
-    Failure,
+  Initialized,
+  Fetching,
+  Ready,
+  Failure,
 }
 
 interface Config {
-    number: number;
-    init: boolean;
+  number: number;
+  init: boolean;
 }
 
 interface State {
-    status: ConfigStatus;
-    config?: Config;
+  status: ConfigStatus;
+  config?: Config;
 }
 
 interface Initialized {
-    status: ConfigStatus.Initialized;
-    config?: Config;
+  status: ConfigStatus.Initialized;
+  config?: Config;
 }
 
 interface Fetching {
-    status: ConfigStatus.Fetching;
+  status: ConfigStatus.Fetching;
 }
 
 interface Ready {
-    status: ConfigStatus.Ready;
+  status: ConfigStatus.Ready;
 }
 
 interface Failure {
-    status: ConfigStatus.Failure;
-    config?: undefined;
+  status: ConfigStatus.Failure;
+  config?: undefined;
 }
 
 export type Action = Initialized | Fetching | Ready | Failure;
 
 function configReducer(state: State, action: Action): State {
-    console.log("configReducer", state, action);
-    switch (action.status) {
-        case ConfigStatus.Ready:
-        case ConfigStatus.Initialized: {
-            return {...state, ...action};
-        }
-        case ConfigStatus.Failure: {
-            if (state.status === ConfigStatus.Fetching) {
-                return {...state, ...action};
-            } else {
-                return state;
-            }
-        }
-        case ConfigStatus.Fetching: {
-            return {
-                ...state,
-                ...action,
-            };
-        }
+  console.log("configReducer", state, action);
+  switch (action.status) {
+    case ConfigStatus.Ready:
+    case ConfigStatus.Initialized: {
+      return {...state, ...action};
     }
+    case ConfigStatus.Failure: {
+      if (state.status === ConfigStatus.Fetching) {
+        return {...state, ...action};
+      }
+      return state;
+    }
+    default:
+    case ConfigStatus.Fetching: {
+      return {
+        ...state,
+        ...action,
+      };
+    }
+  }
 }
 
 type Dispatch = (action: Action) => void;
+
 interface StateContextType {
-    state: State;
-    dispatch: Dispatch;
+  state: State;
+  dispatch: Dispatch;
 }
+
 // const StateContext = createContext<State | undefined>(undefined);
 // const StateContext = createContext<StateContextType>({state: {status: ConfigStatus.Fetching}, dispatch: (action: Action) => {}});
-const StateContext = createContext<StateContextType|undefined>(undefined);
+const StateContext = createContext<StateContextType | undefined>(undefined);
 
 // const DispatchContext = createContext<Dispatch | undefined>(undefined);
 const CountProvider = ({children}: Props): JSX.Element => {
-    const [count, setCount] = useState(0);
-    const [state, dispatch] = useReducer(configReducer, {
-        status: ConfigStatus.Fetching,
-    });
-    console.log('CountProvider', state);
+  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(configReducer, {
+    status: ConfigStatus.Fetching,
+  });
+  console.log('CountProvider', state);
 
-    const plusCount = (): void => {
-        setCount(count + 1);
-    };
+  const plusCount = (): void => {
+    setCount(count + 1);
+  };
 
-    return (
-        <StateContext.Provider value={{state, dispatch}}>
-            <CountContext.Provider
-                value={{
-                    count,
-                    plusCount,
-                }}>
-                {children}
-            </CountContext.Provider>
-        </StateContext.Provider>
-    );
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return (
+      <StateContext.Provider value={{state, dispatch}}>
+        <CountContext.Provider
+          value={{
+            count,
+            plusCount,
+          }}>
+          {children}
+        </CountContext.Provider>
+      </StateContext.Provider>
+  );
 };
 
 function useConfigDispatch() {
-    const stateContext = useContext(StateContext);
-    const dispatch = stateContext?.dispatch;
-    if (!dispatch) {
-        throw new Error(`invalid dispatch`);
-    }
+  const stateContext = useContext(StateContext);
+  const dispatch = stateContext?.dispatch;
+  if (!dispatch) {
+    throw new Error(`invalid dispatch`);
+  }
 
-    // return () => {dispatch({
-    //     status: ConfigStatus.Ready
-    // })};
-    return dispatch;
+  // return () => {dispatch({
+  //     status: ConfigStatus.Ready
+  // })};
+  return dispatch;
 }
 
 export {CountContext, CountProvider, useConfigDispatch};
